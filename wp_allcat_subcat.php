4665d0daaf798dc719fbc6aadb78afad567fee9d
namasteashoka <ashok@stellardigital.in>
2017-07-28 16:22:38 +0530

    Commit After Changes

--- wp_allcat_subcat.php
+++ wp_allcat_subcat.php
@@ -1,43 +1,37 @@
 <?php
 include "wp-load.php";
-// prior to wordpress 4.5.0
-$args = array(
-    'number'     => $number,
-    'orderby'    => $orderby,
-    'order'      => $order,
-    'hide_empty' => $hide_empty,
-    'include'    => $ids
-);
 
-$product_categories = get_terms( 'product_cat', $args );
+function GetAllCategoryWP(){
+		
+		$args = array(
+		'number'     => $number,
+		'orderby'    => $orderby,
+		'order'      => $order,
+		'hide_empty' => $hide_empty,
+		'include'    => $ids
+		);
 
-// since wordpress 4.5.0
-$args = array(
-    'taxonomy'   => "product_cat",
-    'number'     => $number,
-    'orderby'    => $orderby,
-    'order'      => $order,
-    'hide_empty' => $hide_empty,
-    'include'    => $ids,
-	'url'		 =>	$url
-);
-$product_categories = get_terms($args);
+	$categories = get_terms( 'product_cat', $args );
 
-echo "<pre>";
-print_r($product_categories);
-echo "</pre>";
 
-//$category_id
+	$args = array(
+		'taxonomy'   => "product_cat",
+		'number'     => $number,
+		'orderby'    => $orderby,
+		'order'      => $order,
+		'hide_empty' => $hide_empty,
+		'include'    => $ids,
+		'url'		 =>	$url
+	);
+	$categories = get_terms($args);
 
-$category_link = get_category_link(964);
-
-//$items = array();
-$i=1;
-foreach($product_categories as $username) {
- $items = $username->term_id;
- echo $category_link = get_category_link($items);
- echo "</br>";
- $i++;
+	$i=1;
+	foreach($categories as $username) {
+	 $items = $username->term_id;
+	 echo $category_link = get_category_link($items);
+	 echo "</br>";
+	 $i++;
+		}
 }
 
-//print_r($items);
+GetAllCategoryWP();
